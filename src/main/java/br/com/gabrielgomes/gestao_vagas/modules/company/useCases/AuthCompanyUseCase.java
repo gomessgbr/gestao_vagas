5321ccda2605fff484b8aa96b670164b3c37652f
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-01 22:40:50 -0300

    feat: expiration time for token

--- src/main/java/br/com/gabrielgomes/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/gabrielgomes/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.naming.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
 
 @Service
 public class AuthCompanyUseCase {
@@ -31,7 +33,7 @@ public class AuthCompanyUseCase {
             throw new AuthenticationException();
         }
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        return JWT.create().withIssuer("javagas").withSubject(company.getId().toString()).sign(algorithm);
+        return JWT.create().withIssuer("javagas").withExpiresAt(Instant.now().plus(Duration.ofHours(2))).withSubject(company.getId().toString()).sign(algorithm);
 
     }
 
