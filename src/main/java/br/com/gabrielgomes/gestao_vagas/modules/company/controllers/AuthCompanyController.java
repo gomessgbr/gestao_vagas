969703b23641abee94044dba61f8b7829cd43a5c
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-01 22:36:16 -0300

    feat: handle unauthorized auth error

--- src/main/java/br/com/gabrielgomes/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/gabrielgomes/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -3,6 +3,8 @@ package br.com.gabrielgomes.gestao_vagas.modules.company.controllers;
 import br.com.gabrielgomes.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.gabrielgomes.gestao_vagas.modules.company.useCases.AuthCompanyUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +19,16 @@ public class AuthCompanyController {
     @Autowired
     private AuthCompanyUseCase authCompanyUseCase;
     @PostMapping("/company")
-    public String create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
-        return this.authCompanyUseCase.execute(authCompanyDTO);
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO){
+        try{
+            var result = this.authCompanyUseCase.execute(authCompanyDTO);
+            return ResponseEntity.ok().body(result);
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+
+        }
+
 
     }
 
